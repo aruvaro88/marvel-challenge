47cd20e44d8f056b66da76a33f8e5d726a6ec839
aruvaro88 <aruvaro@gmail.com>
2024-03-01 18:03:26 +0100

    view character thumbnail

--- src/components/characterCard/CharacterCard.component.tsx
+++ src/components/characterCard/CharacterCard.component.tsx
@@ -8,7 +8,7 @@ interface CharacterCardProps {
 export const CharacterCard = ({ character }: CharacterCardProps) => {
   return (
     <div>
-      <img src={character.thumbnail.path + character.thumbnail.extension} />
+      <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} />
       <div>{character.name}</div>
     </div>
   )
