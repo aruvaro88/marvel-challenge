6e2c514891ea1ea3ca72be1cf4d3e4132defff22
aruvaro88 <aruvaro@gmail.com>
2024-03-05 23:13:22 +0100

    enviroment vars included

--- src/services/characters/characters.services.ts
+++ src/services/characters/characters.services.ts
@@ -1,13 +1,13 @@
 import { Endpoints } from "../../endpoints/endpoints"
 import { Character } from "../../models/character.model"
 
-const API_BASE_URL = "http://gateway.marvel.com/"
-const API_PUBLIC_KEY = "433360b1ec5781819e23e50d07d7a637"
 const LIMIT = 50
 
 export const fetchCharacters: () => Promise<Character[]> = async () => {
   try {
-    const response = await fetch(API_BASE_URL + Endpoints.getCharacters + `?limit=${LIMIT}&apikey=${API_PUBLIC_KEY}`)
+    const response = await fetch(
+      import.meta.env.VITE_API_BASE_URL + Endpoints.getCharacters + `?limit=${LIMIT}&apikey=${import.meta.env.VITE_API_PUBLIC_KEY}`
+    )
     const data = await response.json()
     return data.data.results
   } catch (error) {
@@ -17,7 +17,11 @@ export const fetchCharacters: () => Promise<Character[]> = async () => {
 
 export const fetchCharactersByName: (name: string) => Promise<Character[]> = async (name) => {
   try {
-    const response = await fetch(API_BASE_URL + Endpoints.getCharacters + `?nameStartsWith=${name}&limit=${LIMIT}&apikey=${API_PUBLIC_KEY}`)
+    const response = await fetch(
+      import.meta.env.VITE_API_BASE_URL +
+        Endpoints.getCharacters +
+        `?nameStartsWith=${name}&limit=${LIMIT}&apikey=${import.meta.env.VITE_API_PUBLIC_KEY}`
+    )
     const data = await response.json()
     return data.data.results
   } catch (error) {
@@ -27,7 +31,11 @@ export const fetchCharactersByName: (name: string) => Promise<Character[]> = asy
 
 export const fetchCharacterDetails: (id: number) => Promise<Character> = async (id) => {
   try {
-    const response = await fetch(API_BASE_URL + Endpoints.getCharacterDetail.replace(":id", id.toString()) + `?apikey=${API_PUBLIC_KEY}`)
+    const response = await fetch(
+      import.meta.env.VITE_API_BASE_URL +
+        Endpoints.getCharacterDetail.replace(":id", id.toString()) +
+        `?apikey=${import.meta.env.VITE_API_PUBLIC_KEY}`
+    )
     const data = await response.json()
     return data.data.results[0]
   } catch (error) {
